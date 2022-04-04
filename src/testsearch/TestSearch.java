57ce064d44a1e17226b77a1c37fa8c8f6293e99d
CompSci2020 <rkaune@ugcloud.ca>
2022-04-04 13:41:39 -0400

    Update header
    UH

--- src/testsearch/TestSearch.java
+++ src/testsearch/TestSearch.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Kaune
+ * April 4,2022
  */
 package testsearch;
 import javax.swing.*;
